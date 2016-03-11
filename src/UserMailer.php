6f26e5bab950446d94180479ada1d872d7ad5d64
lembarek <lembarek2011@gmail.com>
2016-03-11 20:27:49 +0100

    sendResetPasswordEmailTo

--- src/UserMailer.php
+++ src/UserMailer.php
@@ -20,4 +20,19 @@ class UserMailer extends Mailer
         $view = 'mailer::emails.welcome';
         return $this->sendTo($to, $subject, $view);
     }
+
+    /**
+     * to send a message to reset user password
+     *
+     * @param  string  $data
+     * @return mixed
+     */
+    public function sendResetPasswordEmailTo($data)
+    {
+        $subject = "Reset Password";
+        $view = 'mailer::emails.resetPassword';
+        return $this->sendTo($data['email'], $subject, $view, $data);
+    }
+
+
 }
---
+++ src/views/emails/resetPassword.blade.php
@@ -0,0 +1,3 @@
+Please click this link to reset your password
+
+<a href="{{ Route('auth::reset_password', ['email' => $token]) }}">reset your password</a>
